6406422f36b44a0a76b9674f41e4ec7465e84a6d
Julian <julianmuchacho666@gmail.com>
2021-08-24 09:41:47 -0400

    Update news.php

--- news.php
+++ news.php
@@ -59,6 +59,19 @@
 
 		<p style='margin-bottom:0.75rem !important;'>
 			<?php echo $newsContent; ?>
+
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8564713175090072"
+     crossorigin="anonymous"></script>
+<!-- DisplayAd -->
+<ins class="adsbygoogle"
+     style="display:block"
+     data-ad-client="ca-pub-8564713175090072"
+     data-ad-slot="7188111027"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 		</p>
 
 
