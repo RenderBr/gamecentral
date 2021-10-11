3e0857c50e529cc68d05367c1c0477b183d6b20a
Julian <julianmuchacho666@gmail.com>
2021-10-11 11:43:03 -0400

    added ads

--- flashgame.php
+++ flashgame.php
@@ -72,8 +72,19 @@
         });
     </script>
 </div>
-<hr class='mt-2 nav-break'>
-
+<hr class='mt-2 nav-break' style='margin-bottom: 0.3rem !important;'>
+<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8564713175090072"
+crossorigin="anonymous"></script>
+<!-- DisplayAd -->
+<ins class="adsbygoogle"
+style="display:block"
+data-ad-client="ca-pub-8564713175090072"
+data-ad-slot="7188111027"
+data-ad-format="auto"
+data-full-width-responsive="true"></ins>
+<script>
+(adsbygoogle = window.adsbygoogle || []).push({});
+</script>
 
 
 
